fa7a1e99c1a84f278a24b0b6e8cac1ce3d9bc2ac
Ramon Ramon <ramonsramon24@gmail.com>
2019-04-14 17:15:28 -0500

    First Section Complete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAVD4fXeh-MY2qaC-KbQZdfB21UD15Y1h0",
+    authDomain: "oshop-fc0cf.firebaseapp.com",
+    databaseURL: "https://oshop-fc0cf.firebaseio.com",
+    projectId: "oshop-fc0cf",
+    storageBucket: "",
+    messagingSenderId: "216331199100"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVD4fXeh-MY2qaC-KbQZdfB21UD15Y1h0",
+    authDomain: "oshop-fc0cf.firebaseapp.com",
+    databaseURL: "https://oshop-fc0cf.firebaseio.com",
+    projectId: "oshop-fc0cf",
+    storageBucket: "",
+    messagingSenderId: "216331199100"
+  }
 };
 
 /*
