06218f04bb3db98deae0d0229d19b764c4153fdf
Ramon Ramon <44535195+AggieRamon@users.noreply.github.com>
2019-05-27 18:13:17 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAVD4fXeh-MY2qaC-KbQZdfB21UD15Y1h0",
+    apiKey: "",
     authDomain: "oshop-fc0cf.firebaseapp.com",
     databaseURL: "https://oshop-fc0cf.firebaseio.com",
     projectId: "oshop-fc0cf",
